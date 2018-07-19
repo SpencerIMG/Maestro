6d457a6bbfc9ceb6a08e4eb052d0fe33d6494857
Spencer Harder <spencerrift@gmail.com>
2018-07-19 16:29:07 -0500

    Added menu and functionality
    Styled dropdown menu, added js for showing and hiding. About page next.

---
+++ js/MaestroJS.js
@@ -0,0 +1,8 @@
+/*eslint-env browser*/
+
+//DropdownMenu functionality jquery
+$(document).ready(function(){
+    $("#toggleMenu").click(function(){
+        $(".dropdown-menu-maestro").toggle();
+    });
+});
